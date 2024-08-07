4edd9c012ca2a65bf8b4f6e791f21e38d10970b0
SRVNA <srvnasigna2@gmail.com>
2024-08-07 23:45:25 +0530

    Dash Added

--- src/main/java/newbasics/login.java
+++ src/main/java/newbasics/login.java
@@ -1,5 +1,5 @@
 package newbasics;
- 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,35 +7,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
- 
+
 public class login {
     @Test
     public void main() throws InterruptedException {
-        // TODO Auto-generated method stub
+        
         System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+        
+       
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
  
+       
         driver.get("https://signadart.ai/");
- 
-        Thread.sleep(3000);
- 
-        driver.findElement(By.xpath("//span[contains(@class, 'block text-inherit w-full h-full rounded-[50px] text-lg font-bold') and text()='Login']")).click();
- 
-        Thread.sleep(3000);
- 
-        driver.findElement(By.xpath("//input[@name='userName' and @placeholder='Email']")).sendKeys("Testingteam@signatech.com");
- 
-        Thread.sleep(2000);
- 
-        driver.findElement(By.xpath("//input[@name='password' and @placeholder='Password']")).sendKeys("test@123");
- 
-        Thread.sleep(2000);
- 
-        driver.findElement(By.xpath("//button[text()='Sign In']")).click();
- 
-        // Wait for the page to load after login
+        
+       
         WebDriverWait wait = new WebDriverWait(driver, 10);
+        
+        
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'block text-inherit w-full h-full rounded-[50px] text-lg font-bold') and text()='Login']")));
+        loginButton.click();
+        
+        
+        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='userName' and @placeholder='Email']")));
+        emailInput.sendKeys("Testingteam@signatech.com");
+ 
+        
+        WebElement passwordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password' and @placeholder='Password']")));
+        passwordInput.sendKeys("test@123");
+ 
+        
+        WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Sign In']")));
+        signInButton.click();
+ 
+        
         WebElement welcomeTextElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(text(),'Welcome onboard')]")));
  
         // Check if "Welcome onboard" text is present
@@ -45,5 +50,7 @@ public class login {
             System.out.println("Fail");
         }
 
+       
+        driver.quit();
     }
 }
--- src/main/java/newbasics/logout.java
+++ src/main/java/newbasics/logout.java
@@ -1,5 +1,5 @@
 package newbasics;
- 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,43 +7,48 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
- 
+
 public class logout {
-@Test
-	public void main() throws InterruptedException {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-ChromeDriver driver = new ChromeDriver ();
-driver.manage().window().maximize();
+    @Test
+    public void main() throws InterruptedException {
+       
+        System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+        
+      
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
  
-driver.get("https://signadart.ai/");
+        
+        driver.get("https://signadart.ai/");
+        
+       
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        
+        
+        WebElement loginButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'block text-inherit w-full h-full rounded-[50px] text-lg font-bold') and text()='Login']")));
+        loginButton.click();
+        
+      
+        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='userName' and @placeholder='Email']")));
+        emailInput.sendKeys("Testingteam@signatech.com");
  
-Thread.sleep(3000);
+       
+        WebElement passwordInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='password' and @placeholder='Password']")));
+        passwordInput.sendKeys("test@123");
  
-driver.findElement(By.xpath("//span[contains(@class, 'block text-inherit w-full h-full rounded-[50px] text-lg font-bold') and text()='Login']")).click();
+        
+        WebElement signInButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Sign In']")));
+        signInButton.click();
  
-Thread.sleep(3000);
+        
+        WebElement avatarButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(@class, 'avatar') and contains(@class, 'avatar-circle') and contains(@class, 'bg-amber-500')]")));
+        avatarButton.click();
+        
+        
+        WebElement signOutButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Sign Out']")));
+        signOutButton.click();
  
-driver.findElement(By.xpath("//input[@name='userName' and @placeholder='Email']")).sendKeys("Testingteam@signatech.com");
- 
-Thread.sleep(2000);
- 
-driver.findElement(By.xpath("//input[@name='password' and @placeholder='Password']")).sendKeys("test@123");
- 
-Thread.sleep(2000);
- 
-driver.findElement(By.xpath("//button[text()='Sign In']")).click();
- 
-Thread.sleep(4000);
- 
-driver.findElement(By.xpath("//span[contains(@class, 'avatar') and contains(@class, 'avatar-circle') and contains(@class, 'bg-amber-500')]")).click();
- 
-Thread.sleep(3000);
- 
-driver.findElement(By.xpath("//span[text()='Sign Out']")).click();
-Thread.sleep(4000);
- 
-driver.close();
- 
-}
+
+        driver.quit();
+    }
 }
